29e6e7834a35a1055367422492f5a2e1ccc62a44
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-01-07 23:08:02 +0100

    job proposal with threejs

---
+++ src/Olanrewaju.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Olanrewaju = () => {
+    return (
+        <>
+        <div className="heading">
+       <span>hi there. Are you in need of a </span><span>Web Developer ?</span>
+       <p>
+         well look no further i got you covered. my name is OLAYIWOLA Olanrewaju
+         and i am a fullstack developer MERN stack, i pride myself in ability to pick up new 
+         technology quickly.
+       </p>
+       </div>
+     
+        </>
+    );
+};
+
+export default Olanrewaju;
