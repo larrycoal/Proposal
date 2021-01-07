7452f67b3c2196bc955bf32de0e7cbdc87174b55
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-01-08 00:05:04 +0100

    job proposal

--- src/Olanrewaju.js
+++ src/Olanrewaju.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Olanrewaju = () => {
-    return (
-        <>
-        <div className="heading">
-       <span>hi there. Are you in need of a </span><span>Web Developer ?</span>
-       <p>
-         well look no further i got you covered. my name is OLAYIWOLA Olanrewaju
-         and i am a fullstack developer MERN stack, i pride myself in ability to pick up new 
-         technology quickly.
-       </p>
-       </div>
-     
-        </>
-    );
+  return (
+    <>
+      <div className="heading">
+        <span>hi neu. heard you in need of a </span>
+        <span>FRONTEND Developer</span>
+        <p>
+          well look no further i got you covered. my name is OLAYIWOLA
+          Olanrewaju and i am here to fill that position armed with React
+          framework,other technology in my armory include Redux,Tailwind,
+          nodejs,Express,mongoDb,mySql and my tenacity at problem-solving,above all i
+          pride myself in ability to pick up new technology quickly. 
+          <a href="http://olanrewaju.herokuapp.com/">portfolio</a>
+          <a href="https://www.linkedin.com/in/olanrewaju-olayiwola/">linkedin</a>
+          <a href="https://github.com/larrycoal">github</a>
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Olanrewaju;
